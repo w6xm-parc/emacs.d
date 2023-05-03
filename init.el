cd31039db6d4fc400880087bc8b8a29dedbcf2d4
John Weaver <w6xm@palomararc.org>
2023-05-03 13:03:22 -0700

    Tighten up init.el
    * add elisp-autofmt
    * add statement to create tmp dir
    * add .gitignore
    * fix spelling error

--- init.el
+++ init.el
@@ -2,12 +2,12 @@
 (blink-cursor-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(setq inihibit-splash-screen t)
-(setq inihibit-startup-message t)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
 (setq initial-scratch-message "")
-(setq inihibit-startup-echo-area-message "john.weaver")
+(setq inhibit-startup-echo-area-message t)
 (load-theme 'deeper-blue)
-;;;; DO THIS ^^^^^ AS FAST AS POSSIBLE!
+;; ^^^ the clean look
 
 ;; save minibuffer history
 (savehist-mode t)
@@ -16,43 +16,48 @@
 (electric-pair-mode t)
 
 ;; housekeeping
-(setq save-place-file "~/.emacs.d/saved-places")
 (setq tetris-score-file "~/.emacs.d/tetris-scores")
+(setq save-place-file "~/.emacs.d/saved-places")
 (setq temporary-file-directory "~/.emacs.d/tmp")
+(unless (file-directory-p temporary-file-directory)
+  (make-directory temporary-file-directory))
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
-
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;; Package Stuff
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list
+ 'package-archives '("melpa" . "https://melpa.org/packages/"))
 
 ;; This is only needed once, near the top of the file
 (eval-when-compile
   (require 'use-package))
-
 (require 'use-package-ensure)
 (setq use-package-always-ensure t)
 
-(use-package magit
-  :commands magit-status
-  :init
-  (global-set-key (kbd "<f9>") 'magit-status))
-
+;; List of packages
+(use-package ansible-vault)
 (use-package browse-kill-ring)
-(use-package switch-window
-  :commands switch-window
-  :init
-  (global-set-key (kbd "C-x o") 'switch-window))
-
-(use-package json-mode)
-(use-package yaml-mode)
-(use-package terraform-mode)
+(use-package
+ elisp-autofmt
+ :commands (elisp-autofmt-mode elisp-autofmt-buffer)
+ :hook (emacs-lisp-mode . elisp-autofmt-mode))
 (use-package ini-mode)
+(use-package json-mode)
+(use-package
+ magit
+ :commands magit-status
+ :init (global-set-key (kbd "<f9>") 'magit-status))
 (use-package org-bullets)
 (use-package poetry)
-(use-package ansible-vault)
+(use-package
+ switch-window
+ :commands switch-window
+ :init (global-set-key (kbd "C-x o") 'switch-window))
+(use-package terraform-mode)
+(use-package yaml-mode)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
@@ -64,19 +69,27 @@
   (kill-emacs))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(connection-local-criteria-alist
-   '(((:application eshell)
-      eshell-connection-default-profile)) t)
+   '(((:application eshell) eshell-connection-default-profile)))
  '(connection-local-profile-alist
-   '((eshell-connection-default-profile
-      (eshell-path-env-list))) t)
- '(package-selected-packages '(magit)))
+   '((eshell-connection-default-profile (eshell-path-env-list))))
+ '(package-selected-packages
+   '(yaml-mode
+     terraform-mode
+     switch-window
+     poetry
+     org-bullets
+     magit
+     json-mode
+     ini-mode
+     elisp-autofmt
+     browse-kill-ring
+     ansible-vault)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
